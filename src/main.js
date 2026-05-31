f66184cd8075731ae3cf762d01782b02fb6eb372
dmitr9785-sketch <dmitr9785@gmail.com>
2026-05-31 20:58:13 +0300

    RX7 scale 0.05

--- src/main.js
+++ src/main.js
@@ -76,7 +76,7 @@ async function init() {
       model = ponyModel;
     } else if (choice === 'rx7') {
       model = unlockCarModel;
-      playerScale = 0.15;
+      playerScale = 0.05;
     } else {
       model = playerModelRace;
     }
