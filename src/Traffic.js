0725673e1849a81eaee21ce6b8068df4fef26347
dmitr9785-sketch <dmitr9785@gmail.com>
2026-05-31 21:37:04 +0300

    classic_muscle_car: scale 0.5

--- src/Traffic.js
+++ src/Traffic.js
@@ -106,7 +106,7 @@ export class Traffic {
     }
 
     if (car.userData.modelId === 'traffic_9') {
-      car.scale.setScalar(0.6);
+      car.scale.setScalar(0.5);
       car.rotation.set(0, 0, 0);
       car.position.set(x, 0.2, z);
     } else if (car.userData.modelId === 'traffic_10') {
