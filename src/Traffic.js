dd1c828bd3c82ed6426d3305de4e39aa09b5ba51
dmitr9785-sketch <dmitr9785@gmail.com>
2026-05-31 22:26:33 +0300

    traffic_10: scale 0.5

--- src/Traffic.js
+++ src/Traffic.js
@@ -125,7 +125,7 @@ export class Traffic {
       car.rotation.set(0, 0, 0);
       car.position.set(x, 0.1, z);
     } else if (car.userData.modelId === 'traffic_10') {
-      car.scale.setScalar(0.6);
+      car.scale.setScalar(0.5);
       car.rotation.set(0, 0, 0);
       car.position.set(x, 0, z);
     } else {
